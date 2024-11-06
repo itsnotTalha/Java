9b047a4aa84c721a61038948bd98628cc2d8f590
itsnotTalha <talhajubayer737@gmail.com>
2024-11-06 18:18:31 +0600

    Formatted

--- ArraySum/src/SUmArray.java
+++ ArraySum/src/SUmArray.java
@@ -1,15 +1,16 @@
 public class SUmArray {
     public static void main(String[] args) {
-        int x[]={1,6,3}, y[]={4,3,4};
+        int x[] = {1, 6, 3}, y[] = {4, 3, 4};
         //int z[] = new int[3];
-        myf(x,y);
-        for(int i=0; i<3; i++){
+        myf(x, y);
+        for (int i = 0; i < 3; i++) {
             System.out.println(x[i]);
         }
     }
-    public static void myf(int x[], int y[]){
-        for( int i = 0; i < 3; i++){
-            x[i]+=y[i];
+
+    public static void myf(int x[], int y[]) {
+        for (int i = 0; i < 3; i++) {
+            x[i] += y[i];
         }
     }
 }
--- ArraySum/src/sumOfArray.java
+++ ArraySum/src/sumOfArray.java
@@ -1,15 +1,16 @@
 public class sumOfArray {
     public static void main(String[] args) {
-        int x[]={1,6,3}, y[]={4,3,4};
+        int x[] = {1, 6, 3}, y[] = {4, 3, 4};
         int z[] = new int[3];
-        myf(x,y,z);
-        for(int i=0; i<3; i++){
+        myf(x, y, z);
+        for (int i = 0; i < 3; i++) {
             System.out.println(z[i]);
         }
     }
-    public static void myf(int x[], int y[], int z[]){
-        for( int i = 0; i < 3; i++){
-            z[i] = x[i]+y[i];
+
+    public static void myf(int x[], int y[], int z[]) {
+        for (int i = 0; i < 3; i++) {
+            z[i] = x[i] + y[i];
         }
     }
 }
