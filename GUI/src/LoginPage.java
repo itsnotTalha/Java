116952be1e051fa47260ecb194698d990cd0fa65
itsnotTalha <talhajubayer737@gmail.com>
2025-01-16 06:54:20 +0600

    nothing

--- GUI/src/LoginPage.java
+++ GUI/src/LoginPage.java
@@ -9,8 +9,8 @@ public class LoginPage implements ActionListener {
         JTextField t1 = new JTextField();
         JPasswordField p1 = new JPasswordField();
         String userId,password;
-    LoginPage(){
         JFrame frame = new JFrame("MY FRAME");
+    LoginPage(){
         frame.setBounds(100,50,420,420);
         JLabel username = new JLabel("Username");
         JLabel pass = new JLabel("Password");
@@ -40,19 +40,38 @@ public class LoginPage implements ActionListener {
         if(login == e.getSource()){
             userId=t1.getText();
             password = Arrays.toString(p1.getPassword());
-            JFrame frame1 = new JFrame();
-            frame1.setSize(420,420);
-            frame1.add(new JTextField("Login completed Successfully"));
-            frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame1.setVisible(true);
+            frame.add(homepage());
         }
         if(reset==e.getSource()){
             t1.setText("");
             p1.setText("");
         }
     }
+    public Container homepage(){
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(3,2));
+        JButton b1 = new JButton("Save");
+        //JTextField t1,t2,t3 = new JTextField();
+        JLabel l1 = new JLabel("Name: ");
+        JLabel l2 = new JLabel("Age: ");
+        JLabel l3 = new JLabel("Gender: ");
+
+        p1.add(l1);
+        p1.add(new JTextField());
+        p1.add(l2);
+        p1.add(new JTextField());
+        p1.add(l3);
+        p1.add(new JTextField());
+
+        Container c = new Container();
+        c.add(p1,BorderLayout.CENTER);
+        c.add(b1,BorderLayout.SOUTH);
+
+        return c;
+    }
 
     public static void main(String[] args) {
         LoginPage loginPage = new LoginPage();
     }
+
 }
