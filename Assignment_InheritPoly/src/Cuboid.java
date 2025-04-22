67c1d4a571544e29e6f99bb7a458f3d4691f6877
itsnotTalha <talhajubayer737@gmail.com>
2025-04-23 01:06:26 +0600

    Cuboid and Sphere

---
+++ Assignment_InheritPoly/src/Cuboid.java
@@ -0,0 +1,11 @@
+public class Cuboid extends Shape3D{
+    Cuboid(int length, int width, int height){
+        super(length, width, height);
+    }
+    public double volume(){
+        return super.volume();
+    }
+    public double surfaceArea(){
+        return super.surfaceArea();
+    }
+}
---
+++ Assignment_InheritPoly/src/Sphere.java
@@ -0,0 +1,11 @@
+public class Sphere extends Shape3D{
+    Sphere(int length, int width, int height){
+        super(length, width, height);
+    }
+    public double volume(){
+        return super.volume();
+    }
+    public double surfaceArea(){
+        return super.surfaceArea();
+    }
+}
